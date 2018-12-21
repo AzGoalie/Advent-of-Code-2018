dc8a6381f7e3125afe4948404429567a9f1c0871
Travis McMahon <azgoalie@gmail.com>
2018-12-20 23:03:17 -0700

    Day 2 and cleaned up day 1

--- src/aoc/day1.clj
+++ src/aoc/day1.clj
@@ -2,25 +2,21 @@
   (:require [clojure.string :as str]))
 
 ; Part one
-(def frequencies
-  (map #(Long/parseLong %)
-       (str/split-lines (slurp "resources/day-1-input.txt"))))
+(def input
+  (->> (str/split-lines (slurp "resources/day-1-input.txt"))
+       (map #(Long/parseLong %))))
 
-(defn calculate-frequency
-  [frequencies]
+(defn calculate-frequency [frequencies]
   (reduce + frequencies))
 
 ; Part two
-(defn duplicate-frequency
-  [frequencies]
+(defn duplicate-frequency [frequencies]
   (loop [sum 0
          ancestors #{0}
-         current (first frequencies)
-         numbers (rest frequencies)]
+         [current & numbers] (cycle frequencies)]
     (let [newSum (+ sum current)]
       (if (contains? ancestors newSum)
         newSum
         (recur newSum
                (conj ancestors newSum)
-               (first (if (empty? numbers) frequencies numbers))
-               (rest (if (empty? numbers) frequencies numbers)))))))
+               numbers)))))
--- src/aoc/day2.clj
+++ src/aoc/day2.clj
@@ -1,9 +1,29 @@
 (ns aoc.day2
   (:require [clojure.string :as str]))
 
-(def ids (str/split-lines
-           (slurp "resources/day-2-input.txt")))
+(def input (->> (slurp "resources/day-2-input.txt")
+                (str/split-lines)))
 
-(defn checksum
-  [ids-to-check]
-  ())
+; Part One
+(defn has-n [n s]
+  (some #(= n %) (vals (frequencies s))))
+
+(defn count-n [n s]
+  (count (filter #(has-n n %) s)))
+
+(defn checksum [ids]
+  (* (count-n 2 ids) (count-n 3 ids)))
+
+; Part Two
+(defn str-diff [a b]
+  (reduce + (map (fn [c1 c2] (if (= c1 c2) 0 1)) a b)))
+
+(defn remove-diff [a b]
+  (str/join (map (fn [c1 c2] (if (= c1 c2) c1 "")) a b)))
+
+(defn find-box-pair [ids]
+  (->> (for [a ids
+             b ids]
+         (remove-diff a b))
+       (filter #(= (count %) (dec (count (first ids)))))
+       (first)))
