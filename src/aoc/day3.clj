894bb8624ef3751484cddc0f6687cc014b5ad506
Travis McMahon <azgoalie@gmail.com>
2018-12-23 22:34:44 -0700

    Completed day 3

--- src/aoc/day3.clj
+++ src/aoc/day3.clj
@@ -1,12 +1,40 @@
 (ns aoc.day3
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :refer [intersection]]))
 
 (defrecord Claim [id xpos ypos width height])
 
-(defn list-to-claim [[id x y w h]]
+(defn s->claim [[id x y w h]]
   (->Claim id x y w h))
 
 (def input (->> (slurp "resources/day-3-input.txt")
                 (str/split-lines)
                 (map #(re-seq #"\d+" %))
-                (map list-to-claim)))
+                (map (fn [s] (map #(Integer/parseInt %) s)))
+                (map s->claim)))
+
+; Part One
+(defn calc-tiles [claim]
+  (for [x (range (:xpos claim) (+ (:xpos claim) (:width claim)))
+        y (range (:ypos claim) (+ (:ypos claim) (:height claim)))]
+    [x y]))
+
+(defn overlapped-tiles [claims]
+  (->> claims
+       (mapcat calc-tiles)
+       (frequencies)
+       (filter #(> (second %) 1))
+       (map #(first %))))
+
+(defn count-overlap [tiles]
+  (->> (overlapped-tiles tiles)
+       (count)))
+
+; Part Two
+(defn find-non-overlap-claim [claims]
+  (let [overlapped-tiles (overlapped-tiles claims)]
+    (some (fn [claim]
+            (let [tiles (calc-tiles claim)]
+              (when (empty? (intersection (set tiles) (set overlapped-tiles)))
+                (:id claim))))
+          claims)))
